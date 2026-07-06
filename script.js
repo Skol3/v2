2b03f357472a340bd8c2fd21b0592182d9104770
Skol3 <a.gulbinovicius@gmail.com>
2026-07-06 17:59:59 +0300

    Update mobi

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const mobileBtn = document.getElementById("mobileBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-const mobileClose = document.getElementById("mobileClose");
+window.onload = function () {
 
-mobileBtn.onclick = () => {
+    const mobileBtn = document.getElementById("mobileBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+    const mobileClose = document.getElementById("mobileClose");
 
-    mobileMenu.classList.add("show");
+    if (mobileBtn && mobileMenu && mobileClose) {
 
-}
+        mobileBtn.addEventListener("click", function () {
+            mobileMenu.classList.add("show");
+        });
 
-mobileClose.onclick = () => {
+        mobileClose.addEventListener("click", function () {
+            mobileMenu.classList.remove("show");
+        });
 
-    mobileMenu.classList.remove("show");
-
-}
-
-document.querySelectorAll(".mobile-menu a").forEach(link=>{
-
-    link.onclick=()=>{
-
-        mobileMenu.classList.remove("show");
+        document.querySelectorAll(".mobile-menu a").forEach(function(link){
+            link.addEventListener("click", function(){
+                mobileMenu.classList.remove("show");
+            });
+        });
 
     }
 
-});
+};
